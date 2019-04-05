e9588b12165fc0284f6f3cc1562b8ec4e8c37ea7
jason jia <jasonpjia@gmail.com>
2019-04-05 09:57:17 -0500

    test

---
+++ Crawling/main.js
@@ -0,0 +1,93 @@
+
+/*
+algorithm
+
+find the link for books section homepage. (how?)
+grab all relative links that have images, store in queue.
+iterate queue:
+    if link is book product page and hasn't already been scraped, scrape & save data, grab links on page & add to queue.
+    if not book product page, do nothing.
+continue until desired number of books
+
+
+only crawl links that have images on book product pages to maximize the chance of the link being another book product page.
+mainly going for the "Customers who bought this item also bought" links
+*/
+
+
+const cheerio = require("cheerio");
+const {ProxyCrawlAPI} = require('proxycrawl');
+const api = new ProxyCrawlAPI({token: 'HIfghI46G91CCuU5S12Log'});
+const fs = require("fs");
+
+var queue = [];
+var dataArray = [];
+var scrapedURLs = [];
+
+
+function getLinks($) {
+    $("a").has("img").each((i, e) => {
+        // if <a> has href and href is relative
+        if($(e).attr("href") && $(e).attr("href").charAt(0) === "/")
+            queue.push($(e).attr("href"))
+    });
+}
+
+function scrapeBook($) {
+
+    let data = {};
+
+    data.name = $("#productTitle").text();
+
+    data.newPrice = $("#buyNewSection").find(".offer-price").text();
+
+    // How do you get inside an iframe?
+    // data.description = $("#bookDesc_iframe").contents().text();
+
+    data.dimensions = $("#productDetailsTable").find("b:contains('Dimensions')").parent().contents().not("b").text().trim();
+
+    data.imageURLs = [];
+    $(".imageThumb").each((i, e) => {
+        data.imageURLs.push($(e).find("img").attr("src"))
+    });
+
+    data.weight = $("#productDetailsTable").find("b:contains('Weight')").parent().contents().not("b").not("a").text().replace('()', '').trim();
+
+    dataArray.push(data);
+}
+
+
+api.get('https://www.amazon.com/gp/browse.html?node=283155').then(response => {
+
+    getLinks(cheerio.load(response.body));
+
+    // FATAL ERROR: Ineffective mark-compacts near heap limit Allocation failed - JavaScript heap out of memory
+    // while(dataArray.length < 10) {
+        api.get('https://www.amazon.com' + queue[0]).then(response => {
+
+            const $ = cheerio.load(response.body);
+
+            //if this is a book product page (if product details contains ISBN) and it hasn't already been scraped
+            if(!scrapedURLs.includes(queue[0]) && $("#productDetailsTable:contains('ISBN')").length) {
+                scrapeBook($);
+                getLinks($);
+                scrapedURLs.push(queue[0]);
+            }
+
+            queue.shift();
+
+        });
+    // }
+
+    console.log(queue);
+    console.log(dataArray);
+
+    // fs.writeFile("./data.json", JSON.stringify(data, null, 4), (err) => {
+    //     if(err){
+    //         console.error(err);
+    //         return;
+    //     };
+    //     console.log("File has been created");
+    // });
+});
+
